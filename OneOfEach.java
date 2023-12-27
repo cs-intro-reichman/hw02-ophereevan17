7f92f601da265cd562b01b6953e7a872e1ba7df2
ophereevan17 <ophere.evan@post.runi.ac.il>
2023-12-27 19:53:08 +0200

    HW_01

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,33 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+
+		boolean boy = false;
+		boolean girl = false;
+
+		int b = 0;
+		int g = 0;
+
+		while (boy == false || girl == false){
+
+			int chance = (int) ((Math.random() * 10));
+
+			if (chance % 2 == 0){
+				girl = true;
+				g = g + 1;
+				System.out.print("g ");
+			}
+			else{
+				boy = true;
+				System.out.print("b ");
+				b = b + 1;
+			}
+		}
+		int total = g + b;
+		System.out.println();
+		System.out.print("You made it... and you now have " + total + " children.");
 	}
 }
+
+
+
